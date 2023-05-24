170a8e2a14bc9d56e6b21607ab5e7e3a36d7a6e7
lucylanlou <lucy.lou@berkeley.edu>
2023-05-23 17:50:01 -0700

    release 1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({ index, title, icon  }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt className="sm:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full pink-green-gradient p-[1px] rounded-[20px] shadow-card"
