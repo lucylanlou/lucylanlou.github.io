c5bf6c52228aada5e6e455cb8660d51dbdb137c5
lucylanlou <lucy.lou@berkeley.edu>
2024-08-06 16:33:13 -0700

    ball returns after move

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,8 +1,15 @@
-import React, { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
+import React, { forwardRef, Suspense, useRef, useEffect } from "react";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import * as THREE from "three";
 
-import CanvasLoader from '../Loader';
+import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
@@ -15,7 +22,7 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#FFF8FC'
+          color="#FFF8FC"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
@@ -29,23 +36,107 @@ const Ball = (props) => {
         />
       </mesh>
     </Float>
-  )
-}
+  );
+};
+
+const ControlledOrbitControls = () => {
+  const { camera, clock } = useThree();
+  const controlsRef = useRef();
+  const isReturning = useRef(false);
+  const initialPosition = useRef(new THREE.Vector3());
+  const initialQuaternion = useRef(new THREE.Quaternion());
+  const path = useRef([]);
+  const returnStartTime = useRef(0);
+  const returnDuration = 1; // Return duration in seconds
+
+  useFrame(() => {
+    if (controlsRef.current && isReturning.current) {
+      const elapsedTime = clock.getElapsedTime() - returnStartTime.current;
+      const progress = Math.min(elapsedTime / returnDuration, 1);
+
+      if (progress < 1) {
+        const index = Math.floor(progress * (path.current.length - 1));
+        const { position, quaternion } = path.current[index];
+        camera.position.copy(position);
+        camera.quaternion.copy(quaternion);
+      } else {
+        camera.position.copy(initialPosition.current);
+        camera.quaternion.copy(initialQuaternion.current);
+        isReturning.current = false;
+        path.current = [];
+        console.log("Return finished.");
+      }
+      controlsRef.current.update();
+    }
+  });
+
+  useEffect(() => {
+    const controls = controlsRef.current;
+    if (controls) {
+      initialPosition.current.copy(camera.position);
+      initialQuaternion.current.copy(camera.quaternion);
+
+      const onStart = () => {
+        isReturning.current = false;
+        path.current = [];
+        console.log("User interaction started.");
+      };
+
+      const onChange = () => {
+        if (!isReturning.current) {
+          path.current.push({
+            position: camera.position.clone(),
+            quaternion: camera.quaternion.clone(),
+          });
+        }
+      };
+
+      const onEnd = () => {
+        isReturning.current = true;
+        path.current.reverse();
+        returnStartTime.current = clock.getElapsedTime();
+        console.log("User interaction ended. Starting return animation.");
+      };
+
+      controls.addEventListener("start", onStart);
+      controls.addEventListener("change", onChange);
+      controls.addEventListener("end", onEnd);
+
+      return () => {
+        controls.removeEventListener("start", onStart);
+        controls.removeEventListener("change", onChange);
+        controls.removeEventListener("end", onEnd);
+      };
+    }
+  }, [camera, clock]);
+
+  return (
+    <OrbitControls
+      ref={controlsRef}
+      enableZoom={false}
+      enablePan={false}
+      enableRotate={true}
+      enableDamping
+      dampingFactor={0.05}
+    />
+  );
+};
 
 const BallCanvas = ({ icon }) => {
   return (
-  <Canvas
+    <Canvas
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
+      camera={{ position: [0, 0, 5], fov: 75 }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} enablePan={false} />
+        <ControlledOrbitControls />
         <Ball imgUrl={icon} />
       </Suspense>
 
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default BallCanvas
+export default BallCanvas;
